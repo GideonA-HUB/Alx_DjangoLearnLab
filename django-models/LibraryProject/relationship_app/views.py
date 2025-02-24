c82e1a5f75778b8baf8ae1f35bf10b9fb0b2c058
Gideon Amienghemhen <amienz0958@gmail.com>
2025-02-24 15:02:53 +0100

    implementing django views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from relationship_app.models import Book, Library
+from relationship_app.models import Book
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
 
-# Class-based view to show details of a specific library
+from django.views.generic import DetailView
+from relationship_app.models import Library
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
