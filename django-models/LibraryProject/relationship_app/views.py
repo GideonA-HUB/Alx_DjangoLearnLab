1f00b4da4653f8d728801f04923e648639ff0572
Gideon Amienghemhen <amienz0958@gmail.com>
2025-02-25 00:50:59 +0100

    Implementing roled based access control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,8 @@ from .models import Library
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.http import HttpResponse
 
 def list_books(request):
     books = Book.objects.all()
@@ -43,4 +45,25 @@ def user_login(request):
 # User Logout View
 def user_logout(request):
     logout(request)
-    return render(request, 'relationship_app/logout.html')    
+    return render(request, 'relationship_app/logout.html')
+
+def admin_check(user):
+    return user.profile.role == 'Admin'
+
+def librarian_check(user):
+    return user.profile.role == 'Librarian'
+
+def member_check(user):
+    return user.profile.role == 'Member'
+
+@user_passes_test(admin_check)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(librarian_check)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(member_check)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
