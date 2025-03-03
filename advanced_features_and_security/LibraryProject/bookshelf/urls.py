40f2468b2b7fd8c561764bf8341f9c624932470b
Gideon Amienghemhen <amienz0958@gmail.com>
2025-03-03 03:53:02 +0100

    Django security

--- advanced_features_and_security/LibraryProject/bookshelf/urls.py
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.book_list, name='book_list'),  # Book list page
-    path('book/<int:book_id>/', views.book_detail, name='book_detail'),  # Book detail page
+    path('book/<int:book_id>/', views.book_detail, name='book_detail'),
+    path('example/', views.example_view, name='example_form'), 
 ]
