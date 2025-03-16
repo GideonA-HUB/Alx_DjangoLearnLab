7a4671e499271c79a17fcfae967d636cfea67b34
Gideon Amienghemhen <amienz0958@gmail.com>
2025-03-16 02:45:21 +0100

    Implementing filtering, ordering, searching

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
