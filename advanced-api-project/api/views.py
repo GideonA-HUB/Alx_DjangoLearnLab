05ad7a3b3281ac39e9bb2546f6cf952b169e631f
Gideon Amienghemhen <amienz0958@gmail.com>
2025-03-16 02:50:06 +0100

    Implementing filtering, ordering, searching

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,6 @@
 from rest_framework import generics
+from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import OrderingFilter
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
